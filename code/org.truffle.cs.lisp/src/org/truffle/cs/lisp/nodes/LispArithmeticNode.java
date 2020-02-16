7843fb5fbe87397773761faf4de8f386da411989
Volodymyr Chernetskyi <v.v.chernetskyi@gmail.com>
2020-02-16 20:31:23 +0200

    Change arithmetic operator behavior to match lisp

--- code/org.truffle.cs.lisp/src/org/truffle/cs/lisp/nodes/LispArithmeticNode.java
+++ code/org.truffle.cs.lisp/src/org/truffle/cs/lisp/nodes/LispArithmeticNode.java
@@ -10,21 +10,30 @@ import com.oracle.truffle.api.nodes.ExplodeLoop;
 
 public abstract class LispArithmeticNode extends LispOperationNode {
 	private Object accumulator;
+	protected float defaultAccumulator;
 	
 	public abstract Object reduce(Object accumulator, Object value);
 	
 	@Override
 	@ExplodeLoop()
 	public Object execute(VirtualFrame frame) {
-		if (arguments.length > 0) {
-			accumulator = arguments[0].execute(frame);
-		} else {
+		if (arguments.length == 0) {
 			CompilerDirectives.transferToInterpreter();
 			throw new Error("Operation must be have at least one argument.");
 		}
-		
+
+		int start;
+		if (arguments.length == 1) {
+			accumulator = defaultAccumulator;
+			start = 0;
+		} else {
+			accumulator = arguments[0].execute(frame);
+			start = 1;
+		}
+
 		// TODO add checks
-		for (int i = 1; i < arguments.length; i++) {
+
+		for (int i = start; i < arguments.length; i++) {
 			accumulator = reduce(accumulator, arguments[i].execute(frame));
 		}
 		
@@ -32,6 +41,10 @@ public abstract class LispArithmeticNode extends LispOperationNode {
 	}
 	
 	public static class LispAddNode extends LispArithmeticNode {
+		public LispAddNode() {
+			defaultAccumulator = 0;
+		}
+
 		@Override
 		public Object reduce(Object accumulator, Object value) {
 			return (float) accumulator + (float) value;
@@ -39,6 +52,10 @@ public abstract class LispArithmeticNode extends LispOperationNode {
 	}
 	
 	public static class LispSubtractNode extends LispArithmeticNode {
+		public LispSubtractNode() {
+			defaultAccumulator = 0;
+		}
+
 		@Override
 		public Object reduce(Object accumulator, Object value) {
 			return (float) accumulator - (float) value;
@@ -46,6 +63,10 @@ public abstract class LispArithmeticNode extends LispOperationNode {
 	}
 	
 	public static class LispMultiplyNode extends LispArithmeticNode {
+		public LispMultiplyNode() {
+			defaultAccumulator = 1;
+		}
+
 		@Override
 		public Object reduce(Object accumulator, Object value) {
 			return (float) accumulator * (float) value;
@@ -53,6 +74,10 @@ public abstract class LispArithmeticNode extends LispOperationNode {
 	}
 	
 	public static class LispDivideNode extends LispArithmeticNode {
+		public LispDivideNode() {
+			defaultAccumulator = 1;
+		}
+
 		@Override
 		public Object reduce(Object accumulator, Object value) {
 			return (float) accumulator / (float) value;
